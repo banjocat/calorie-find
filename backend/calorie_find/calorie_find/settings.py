7371451947fcb25225bfe44de56722783f0e9939
Jack Muratore <jackmuratore@gmail.com>
2016-07-10 22:16:51 -0400

    Move to postgresql.. try out compose based migrations

--- backend/calorie_find/calorie_find/settings.py
+++ backend/calorie_find/calorie_find/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'calorie_find.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'food',
+        'USER': 'food',
+        'PASSWORD': 'food',
+        'HOST': 'db'
     }
 }
 
