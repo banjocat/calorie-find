53b7ab4f36f716edc6dc462fd2b64035c90ab48c
Jack Muratore <jackmuratore@gmail.com>
2016-07-30 21:16:40 -0400

    Add deploy to tasks

--- tasks.py
+++ tasks.py
@@ -52,4 +52,12 @@ def clean(ctx):
     ctx.run("docker rm $(docker ps -a | awk '/Exited/ {print $1}')")
 
 
+@task
+def deploy(ctx):
+    '''
+    Deploys to staging
+    '''
+    ctx.run('ansible-playbook -i ./ansible/staging_inventory.ini -u root --tags "deploy" --extra-vars="docker_user=$DOCKER_USER docker_password=$DOCKER_PASS docker_email=$DOCKER_EMAIL" ./ansible/playbook.yml')
+
+
 
