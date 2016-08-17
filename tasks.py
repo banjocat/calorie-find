e1d399956ecf21e60d946ed77700c49a1dd9f22e
Jack Muratore <jackmuratore@gmail.com>
2016-08-16 21:55:02 -0400

    Remove compose role and cleanup

--- tasks.py
+++ tasks.py
@@ -44,7 +44,16 @@ def deploy(ctx):
     '''
     Deploys to staging
     '''
-    ctx.run('ansible-playbook -i ./ansible/staging_inventory.ini -u root --tags "deploy" --extra-vars="docker_user=$DOCKER_USER docker_password=$DOCKER_PASS docker_email=$DOCKER_EMAIL" ./ansible/playbook.yml')
+    ctx.run(
+            'ansible-playbook -i '
+            './ansible/staging_inventory.ini '
+            '-u root '
+            '--tags "deploy" '
+            '--extra-vars="'
+            'docker_user=$DOCKER_USER '
+            'docker_password=$DOCKER_PASS '
+            'docker_email=$DOCKER_EMAIL" '
+            './ansible/playbook.yml')
 
 
 
