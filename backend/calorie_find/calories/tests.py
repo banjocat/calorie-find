32578683d81630345e35718d6f4deecf01b5ee60
Jack Muratore <jackmuratore@gmail.com>
2016-08-22 21:55:37 -0400

    More unit tests

--- backend/calorie_find/calories/tests.py
+++ backend/calorie_find/calories/tests.py
@@ -18,22 +18,35 @@ class RestTest(TestCase):
         food = Food(name='nonfat icecream', calories=12.0)
         food.save()
 
-
-    def test_single_item_one_match(self):
+    def _response(self, food_list):
         factory = APIRequestFactory()
         view = CalorieView.as_view()
         data =  {
-                "foods": ['pizza']
+                "foods": [food_list]
                 }
         request = factory.post('/api/v1/calories/', data, format='json')
         response = view(request, data)
         self.assertTrue(response.status_code, 200)
-        logging.debug('Food found %s' % response.data)
-        cheese = json.loads(response.data)['foods']
+        return json.loads(response.data)['foods']
+
+
+    def test_finding_nothing(self):
+        cheese = self._response(['lolnothing'])
         self.assertTrue(cheese)
-        self.assertEquals(1, len(cheese))
+        self.assertEquals(0, len(cheese[0]))
+
+    def test_single_item_one_match(self):
+        cheese = self._response(['pizza'])
+        self.assertTrue(cheese)
+        self.assertEquals(1, len(cheese[0]), msg="Size of %s" % cheese)
         self.assertEquals(10.0, cheese[0][0]['calories'])
 
 
+    def test_multiple_match(self):
+        icecreams = self._response('icecream')
+        self.assertTrue(icecreams)
+        self.assertEquals(2, len(icecreams[2]))
+        self.asssertEquals(-1, icecreams[0][0]['calories'])
+        self.asssertEquals(12.0, icecreams[0][1]['calories'])
 
 
