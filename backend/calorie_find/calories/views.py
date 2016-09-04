54716cfc13c89fafd751596b11470508ddba102c
Jack Muratore <jackmuratore@gmail.com>
2016-09-04 16:23:10 -0400

    Limit query per food to 5

--- backend/calorie_find/calories/views.py
+++ backend/calorie_find/calories/views.py
@@ -29,7 +29,7 @@ class CalorieView(APIView):
             raise Http404
         food_querysets = {"foods": []}
         for food in request.data.get('foods'):
-            q = Food.objects.filter(name__icontains=food)
+            q = Food.objects.filter(name__icontains=food)[:5]
             food_querysets['foods'].append(q)
 
         serializer = FoodListSerializer(food_querysets)
