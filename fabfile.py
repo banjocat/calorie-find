50aa7248ef8445937eef6fd942b20132f0724458
Jack Muratore <jackmuratore@gmail.com>
2016-11-06 10:04:26 -0500

    Change name from prod to staging and fix default login for postgres

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,14 @@ from fabric.api import env, run, put, hosts, cd, local
 
 env.user = 'root'
 
+
 @hosts('giantgreendinosaur.com')
 def deploy():
     '''
     Deploy it
     '''
     run('mkdir -p /app/caloriefind')
-    put('./production-compose.yml', '/app/caloriefind/docker-compose.yml')
+    put('./staging-compose.yml', '/app/caloriefind/docker-compose.yml')
     with cd('/app/caloriefind'):
         run('docker-compose pull')
         run('docker-compose down')
